9ae3ecb5899d64a750e66a5a78325ec90fe4bfda
oksinlove <a.smile.of.fortune@gmail.com>
2016-06-28 20:47:44 +0300

    - 2 commodity photos changed - image_first.js modified

--- js/image_first.js
+++ js/image_first.js
@@ -2,7 +2,22 @@
  * Created by Окси on 16.06.2016.
  */
 (function (window, $) {
-    $("#man").load(function(){
-        jQuery("#navbar").toggle();
+    $(function () {
+        
+        $('#man ').on('load', function(alert){
+            $(".navbar").toggleClass('.show');
+            $(".arrow").toggleClass('.show');
+            $("#hr_man").toggleClass('.show');
+            return false;
+        });
     });
-} (window, jQuery));
+})(window, jQuery);
+/*$(document).ready(function(){
+    $('#man ').on('load', function(){
+        $(".navbar").toggleClass('.show');
+        $(".arrow").toggleClass('.show');
+        $("#hr_man").toggleClass('.show');
+        return false;
+    });
+});*/
+
