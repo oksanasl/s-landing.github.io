a9ff7187a2a6f5d3ffe1efe41d5489355861aef7
oksinlove <a.smile.of.fortune@gmail.com>
2016-06-28 22:27:19 +0300

    language.js added

---
+++ js/language.js
@@ -0,0 +1,18 @@
+/**
+ * Created by Окси on 28.06.2016.
+ */
+/**
+ * Landing page language translator
+ *
+ * @param langFlag
+ */
+function translator (langFlag) {
+    $("html").attr('lang', langFlag);
+    var dataAttr = 'data-lang' + langFlag;
+    var tags = $("[" + dataAttr + "]");
+    $(tags).each(function(){
+        $(this).html(
+            $(this).data(langFlag)
+        );
+    });
+};
