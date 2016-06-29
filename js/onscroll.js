751d79f1bb3ec91ad88cd18dd5b0ce1d7691ac06
LaryShadow <lazarevuch@gmail.com>
2016-06-29 19:00:15 +0300

    - remove unworking hideOff.js  - fix onscroll.js syntax error

--- js/onscroll.js
+++ js/onscroll.js
@@ -3,12 +3,16 @@ function onScroll(event){
 		$('nav a').each(function () {
 			var currentLink = $(this);
 			var refElement = $(currentLink.attr("href"));
-			if (refElement.position().top <= scrollPosition && refElement.position().top + refElement.height() > scrollPosition) {
-				$("body nav #myNavbar1 #myNavbar ul li a").removeClass("active");
-				currentLink.addClass("active");
-			}
-			else{
-				currentLink.removeClass("active");
+
+			if (refElement.position()) {
+				if (refElement.position().top <= scrollPosition
+					&& refElement.position().top + refElement.height() > scrollPosition) {
+					$("body nav #myNavbar1 #myNavbar ul li a").removeClass("active");
+					currentLink.addClass("active");
+				}
+				else{
+					currentLink.removeClass("active");
+				}
 			}
 		});
 	}
