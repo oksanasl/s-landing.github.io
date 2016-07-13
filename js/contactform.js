a3d5b29cb572815bfa72a158eb199c5539ee1bd1
oksinlove <a.smile.of.fortune@gmail.com>
2016-07-13 17:29:02 +0300

    Contact form modified:  - ajax sender fixing - now ajax request for sending email is working properly

--- js/contactform.js
+++ js/contactform.js
@@ -1,20 +1,26 @@
 /**
  * Created by Окси on 12.07.2016.
  */
-document.getElementById('callback').addEventListener('submit', function(evt){
-    var http = new XMLHttpRequest(), f = this;
-    evt.preventDefault();
-    http.open("POST", "/email_sender.php", true);
-    http.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-    http.send("Name=" + f.Name.value + "&Telephone_number=" + f.Telephone_number.value + "&mail=" + f.mail.value + "&Comment=" + f.Comment.value);
-    http.onreadystatechange = function() {
-        if (http.readyState == 4 && http.status == 200) {
-            alert(http.responseText + ', Ваше повідомлення отримано.\nНаші специалисти дадуть відповідь Вам протягом 10 хв.\nМи вдячні Вам за інтерес до нашої компанії!');
-            f.messageFF.removeAttribute('value'); // очистить поле сообщения (две строки)
-            f.messageFF.value='';
+jQuery(document).ready(function () {
+    document.addEventListener('submit', function(evt){
+        var http = new XMLHttpRequest(), f = this;
+        evt.preventDefault();
+        http.open("POST", "http://cten.com.ua/email_sender.php", true);
+        http.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+        http.send(
+            "Name=" + f.getElementById('Name').value 
+            + "&Telephone_number=" + f.getElementById('Telephone_number').value 
+            + "&mail=" + f.getElementById('mail').value 
+            + "&Comment=" + f.getElementById('Comment').value);
+        http.onreadystatechange = function() {
+            if (http.readyState == 4 && http.status == 200) {
+                alert(http.responseText + ', Ваше повідомлення отримано.\nНаші специалисти дадуть відповідь Вам протягом 10 хв.\nМи вдячні Вам за інтерес до нашої компанії!');
+                f.messageFF.removeAttribute('value'); // очистить поле сообщения (две строки)
+                f.messageFF.value='';
+            }
         }
-    }
-    http.onerror = function() {
-        alert('Вибачте, дані не були передані');
-    }
-}, false);
+        http.onerror = function() {
+            alert('Вибачте, дані не були передані');
+        }
+    }, false);
+});
