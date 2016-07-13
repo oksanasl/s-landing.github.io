8eb139e09528886716f74eff34f25b9ff979d892
oksinlove <a.smile.of.fortune@gmail.com>
2016-07-13 16:15:41 +0300

    Contact form modified:  - adds back redirect and logging mail sending fails

--- email_sender.php
+++ email_sender.php
@@ -1,14 +1,26 @@
 <?php
 if (array_key_exists('Subject', $_POST)) {
+   $logFile = 'var/logs/emailNotSent.log';
+
    $to = 'a.smile.of.fortune@gmail.com';
-   $subject = 'Заповнена контактна форма з '.$_SERVER['HTTP_REFERER'];
-   $subject = "=?utf-8?b?". base64_encode($subject) ."?=";
-   $message = "Ім'я: ".$_POST['Name']."\nТелефон: ".$_POST['Subject']."\nE-mail: ".$_POST['E-mail']."\nIP: ".$_SERVER['REMOTE_ADDR']."\nКоментар: ".$_POST['Comment'];
+   $subject = 'Заповнена контактна форма з ' . $_SERVER['HTTP_REFERER'];
+   $subject = "=?utf-8?b?" . base64_encode($subject) . "?=";
+   $message = "Ім'я: " . $_POST['Name'] . "\nТелефон: " . $_POST['Subject']
+                . "\nE-mail: " . $_POST['E-mail'] . "\nIP: " . $_SERVER['REMOTE_ADDR']
+                . "\nКоментар: " . $_POST['Comment'];
    $headers = 'Content-type: text/plain; charset="utf-8"';
    $headers .= "MIME-Version: 1.0\r\n";
    $headers .= "Date: ". date('D, d M Y h:i:s O') ."\r\n";
-   mail($to, $subject, $message, $headers);
-   var_dump($_SERVER);
-   header($_SERVER['HTTP_URI']);
+
+   try {
+      mail($to, $subject, $message, $headers);
+      header('Location: ' . $_SERVER['HTTP_REFERER'] . '#ContactForm');
+   } catch (Exception e){
+        $logMsg = date();
+        $logMsg .= 'n\r\' . $_POST['E-mail'] . ' ' . $__POST['Subject'];
+        $logMsg .= 'n\r\' . $e->getMessage();
+        $logMsg .= '----------------------------------------------------';
+        file_put_contents($logFile, $logMsg, 'FILE_APPEND');
+   }
 }
 ?>
