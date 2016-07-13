1b5be84beb6e857795de7b53b7c49dc3354c63be
oksinlove <a.smile.of.fortune@gmail.com>
2016-07-13 15:05:35 +0300

    Contact form modified:  - email_sender form fields edited;  - index form attributes commented;

--- email_sender.php
+++ email_sender.php
@@ -1,9 +1,10 @@
-<?
-if (array_key_exists('Telephone_number', $_POST)) {
+<?php
+ print_r($__POST);
+if (array_key_exists('Subject', $_POST)) {
    $to = 'a.smile.of.fortune@gmail.com';
    $subject = 'Заповнена контактна форма з '.$_SERVER['HTTP_REFERER'];
    $subject = "=?utf-8?b?". base64_encode($subject) ."?=";
-   $message = "Ім'я: ".$_POST['Name']."\nТелефон: ".$_POST['Telephone_number']."\nE-mail: ".$_POST['mail']."\nIP: ".$_SERVER['REMOTE_ADDR']."\nКоментар: ".$_POST['Comment'];
+   $message = "Ім'я: ".$_POST['Name']."\nТелефон: ".$_POST['Subject']."\nE-mail: ".$_POST['E-mail']."\nIP: ".$_SERVER['REMOTE_ADDR']."\nКоментар: ".$_POST['Comment'];
    $headers = 'Content-type: text/plain; charset="utf-8"';
    $headers .= "MIME-Version: 1.0\r\n";
    $headers .= "Date: ". date('D, d M Y h:i:s O') ."\r\n";
