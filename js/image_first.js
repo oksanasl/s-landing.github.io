cb26c6f4e888e492d5f70710a4822ef907182352
oksinlove <a.smile.of.fortune@gmail.com>
2016-07-14 16:41:34 +0300

    image_first.js commented

--- js/image_first.js
+++ js/image_first.js
@@ -1,26 +1,26 @@
 /**
  * Created by Окси on 16.06.2016.
  */
-document.getElementById('man').style.backgroundImage.load(function() {
-    $(".notmain").attr('src', function() {
-        return $(this).attr("data-src");
-    });
-});
-
-var img = new Image();
-img.src = 'images/TopBanner0.jpg';
-
-var int = setInterval(function() {
-    if (img.complete) {
-        clearInterval(int);
-        document.getElementsByTagName('man')[0].style.backgroundImage = 'images/TopBanner0.jpg(' + img.src + ')';
-    }
-}, 50);
-
-setInterval(function() {
-    var img = new Image();
-    img.src = "images/TopBanner0.jpg"
-    img.onload=function(){$(img).fadeIn(500);}
-    img.style.display="none";
-    document.getElementById('parent').appendChild(img);
-}, 500);
+// document.getElementById('man').style.backgroundImage.load(function() {
+//     $(".notmain").attr('src', function() {
+//         return $(this).attr("data-src");
+//     });
+// });
+//
+// var img = new Image();
+// img.src = 'images/TopBanner0.jpg';
+//
+// var int = setInterval(function() {
+//     if (img.complete) {
+//         clearInterval(int);
+//         document.getElementsByTagName('man')[0].style.backgroundImage = 'images/TopBanner0.jpg(' + img.src + ')';
+//     }
+// }, 50);
+//
+// setInterval(function() {
+//     var img = new Image();
+//     img.src = "images/TopBanner0.jpg"
+//     img.onload=function(){$(img).fadeIn(500);}
+//     img.style.display="none";
+//     document.getElementById('parent').appendChild(img);
+// }, 500);
