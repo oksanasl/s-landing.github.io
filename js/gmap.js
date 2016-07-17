d83555700f6edaedd00e98431941cacf6a8e116b
oksinlove <a.smile.of.fortune@gmail.com>
2016-07-17 18:27:06 +0300

    googleMap display fixing: - gmap.js tag completely changed

--- js/gmap.js
+++ js/gmap.js
@@ -1,16 +1,19 @@
-function initialize() {
-  var mapProp = {
-    center:new google.maps.LatLng(49.576656, 25.624144), 
-    zoom:15,
+function initMap() {
+  var myLatLng = {lat: 49.576656, lng: 25.624144};
+
+  var map = new google.maps.Map(document.getElementById('gMap'), {
+    zoom: 15,
     scrollwheel: false,
-	mapTypeId:google.maps.MapTypeId.ROADMAP
-  };
-  var map=new google.maps.Map(document.getElementById("gMap"),mapProp);
-  
-  var marker = new google.maps.Marker({
-    position: new google.maps.LatLng(49.576656, 25.624144),
-    map: map,
-    title: "СТЕН - Будматеріали гуртом та в роздріб"
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    center: myLatLng
   });
+
+  var marker = new google.maps.Marker({
+    position: myLatLng,
+    map: map,
+    title: 'СТЕН - Будматеріали гуртом та в роздріб'
+  });
+
+  // To add the marker to the map, call setMap();
+  marker.setMap(map);
 }
-google.maps.event.addDomListener(window, "load", initialize);
