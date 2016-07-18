a00e8c65fe8d3aa2625b68ba63a509297c6de026
oksinlove <a.smile.of.fortune@gmail.com>
2016-07-18 12:59:49 +0300

    googleMapMARKER display fixing: - google places reference changed - gmap.js commented

--- js/gmap.js
+++ js/gmap.js
@@ -1,21 +1,33 @@
-function initialize() {
-  var LatLng = {lat: 49.576656, lng: 25.624144};
+// function initializeMapMarker() {
+//   var LatLng = {lat: 49.576656, lng: 25.624144};
+//
+//   var mapProp = {
+//     center:new google.maps.LatLng(49.576656, 25.624144),
+//     zoom:15,
+//     scrollwheel: false,
+//     mapTypeId:google.maps.MapTypeId.ROADMAP
+//   };
+//   var map=new google.maps.Map(document.getElementById("Map"),mapProp);
+//
+//   var marker = new google.maps.Marker({
+//     position: new google.maps.LatLng(49.576656, 25.624144),
+//     map: map,
+//     title: "СТЕН - Будматеріали гуртом та в роздріб"
+//   });
+// }
+// google.maps.event.addDomListener(window, "load", initialize);
 
-  var mapProp = {
-    center:new google.maps.LatLng(49.576656, 25.624144),
-    zoom:15,
-    scrollwheel: false,
-    mapTypeId:google.maps.MapTypeId.ROADMAP
-  };
-  var map=new google.maps.Map(document.getElementById("Map"),mapProp);
+// function CustomMarker(latlng, map) {
+//   this.latlng = latlng;
+//   this.setMap(map);
+// }
+// CustomMarker.prototype = new google.maps.OverlayView();
 
-  var marker = new google.maps.Marker({
-    position: new google.maps.LatLng(49.576656, 25.624144),
-    title: "СТЕН - Будматеріали гуртом та в роздріб"
-  });
-
-  // To add the marker to the map, call setMap();
-  marker.setMap(map);
-}
-
-google.maps.event.addDomListener(window, "load", initialize);
+// function addMarker () {
+//
+//   var marker = new google.maps.Marker(document.getElementById("Map")({
+//     position: new google.maps.LatLng(49.576656, 25.624144),
+//   });
+//
+//   marker.setMap(map);
+// }
