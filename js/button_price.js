9e92eafcc0d0709f5eab5e4a1d1de32b47da7f47
oksinlove <a.smile.of.fortune@gmail.com>
2016-07-19 17:23:54 +0300

    - ContactForm jquery validation added - button_price.js added

--- js/button_price.js
+++ js/button_price.js
@@ -1,3 +1,13 @@
-/**
- * Created by Окси on 19.07.2016.
- */
+// var scrollPosition = $(document).scrollTop();
+// $('nav a').each(function () {
+//     var currentLink = $(this);
+//     var refElement = $(currentLink.attr("href"));
+//     if (refElement.position().top <= scrollPosition && refElement.position().top + refElement.height() > scrollPosition) {
+//         $('body #MainMenu nav ul li a').removeClass("active");
+//         currentLink.addClass("active");
+//     }
+//     else{
+//         currentLink.removeClass("active");
+//     }
+// });
+
---
+++ js/formvalidation.js
@@ -0,0 +1,40 @@
+$(document).ready(function() {
+    $('#submit').click(function(e) {
+        var name = $('#Name').val();
+        var tel = $('#Telephone_number').val();
+        var mail = $('#mail').val();
+
+        var name_regex = /^[A-Za-zА-Яа-яіїє-]+$/;
+        var tel_regex = /{10,}+$/;
+        var email_regex = /^[\w\-\.\+]+\@[a-zA-Zа-яА-Яіїє0-9\.\-]+\.[a-zA-zа-я0-9]{2,4}$/;
+                            // /^([a-zа-яіїє0-9_\.-]+)@([a-а-я-9_\.-]+)\.([a-z\.]{2,6})$/
+        if (name.length == 0) {
+            $('#rule').text("Усі поля відмічені * є обов'язковими для заповнення");
+            $("#name").focus();
+            return false;
+        }
+
+        else if (!name.match(name_regex) || name.length == 0) {
+            $('#r1').text("Використовуйте лише букви");
+            $("#name").focus();
+            return false;
+        }
+
+        else if (!tel.match(tel_regex) || tel.length == 0) {
+            $('#r2').text("Будь ласка, введіть телефон у зазначеному форматі");
+            $("#tel").focus();
+            return false;
+        }
+        else if (!mail.match(email_regex) || mail.length == 0) {
+            $('#r3').text("Будь ласка, введіть дійсну e-mail адресу");
+            $("#mail").focus();
+            return false;
+        }
+ else {
+            $("#modal").modal("show");
+            return true;
+        }
+    });
+});/**
+ * Created by Окси on 19.07.2016.
+ */
