4d59e0c95e69d8367e16e3d787e0f5120c8bb314
oksinlove <a.smile.of.fortune@gmail.com>
2016-07-21 17:27:44 +0300

    priceunblock.js test2

--- js/priceunblock.js
+++ js/priceunblock.js
@@ -1,12 +1,9 @@
-// function DownloadingPriceUnblock(event){
-//     var http = new XMLHttpRequest(), f = this;
-//         if (http.readyState == 4 && http.status == 200) {
-//     $("#downloadPrice"). removeAttribute ("active");
-// }
-// });
-// }
-
-
+function DownloadingPriceUnblock(event){
+    var http = new XMLHttpRequest(), f = this;
+        if (http.readyState == 4 && http.status == 200) {
+            document.getElementById("#downloadPrice"). removeAttribute ("active");
+}
+}
 /**
  * Created by Окси on 20.07.2016.
  */
