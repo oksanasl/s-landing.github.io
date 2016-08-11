c2cbff0df71285e22ef407242b669ac167618f15
oksinlove <a.smile.of.fortune@gmail.com>
2016-08-11 16:25:18 +0300

    first page images synchronous loading enabled

--- js/image_first.js
+++ js/image_first.js
@@ -1,33 +1,4 @@
-// var elements = document.getElementsByClassName("first_hide");
-//
-// document.getElementById("man").style.backgroundImage.onLoad(function(){
-//     elements.removeClass("hide")
-// });
-
-
-var bgImg = $("#man").css("background-image", "url('/images/TopBanner0.jpg')");
-bgImg.onload = function(){
-    $("#man").style.backgroundImage = 'url(' + bgImg.src + ')';
-};
-// bgImg.src = imageLocation;
-
-var listOfId = jQuery("#logo_man, #hr_man, #arrow_banner1, #myNavbar1");
-
-    jQuery("#man").css("background-image", "url('/images/TopBanner0.jpg')").on('load', function() {
-        jQuery.each(listOfId).removeClass("hide");
+ $('#man').ready(function(){
+        $("#logo_man, #hr_man, #arrow_banner1, #myNavbar1").removeClass('hide')
     });
 
-// $(function( )
-// {
-// var background = document.getElementById("#man").style.backgroundImage = 'url(/images/TopBanner0.jpg)';
-//     var elements = document.getElementsByClassName("first_hide");
-//
-//         background.onLoad = function( ){
-//         elements.removeClass("hide")
-//     };
-//         background.src = $("man").css("background-image");
-// });
-
-$(document).ready(function () {
-    jQuery("a[data-slide=next]").trigger("click");
-});
