f5203101061ef0b93c391c9a64e4b47a26457f25
oksinlove <a.smile.of.fortune@gmail.com>
2017-02-19 16:39:59 +0200

    add additional emails, fix log file path

--- email_sender.php
+++ email_sender.php
@@ -1,16 +1,18 @@
 <?php
- $logFile = 'var/logs/emailNotSent.log';
- $to = 'a.smile.of.fortune@gmail.com';
+ $logFile = '/cten.com.ua/www/var/logs/emailNotSent.log';
+ $to = 'a.smile.of.fortune@gmail.com, sten_office@ukr.net, kantornadia@gmail.com';
 
 if (array_key_exists('mail', $_POST)) {
-   $subject = 'Заповнена контактна форма з ' . $_SERVER['HTTP_REFERER'];
-   $subject = "=?utf-8?b?" . base64_encode($subject) . "?=";
-   $message = "Ім'я: " . $_POST['Name'] . "\nТелефон: " . $_POST['Telephone_number']
-                . "\nE-mail: " . $_POST['mail'] . "\nIP: " . $_SERVER['REMOTE_ADDR']
-                . "\nКоментар: " . $_POST['Comment'];
    $headers = 'Content-type: text/plain; charset="utf-8"';
    $headers .= "MIME-Version: 1.0\r\n";
    $headers .= "Date: ". date('D, d M Y h:i:s O') ."\r\n";
+
+   $subject = 'Заповнена контактна форма з ' . $_SERVER['HTTP_REFERER'];
+   $subject = "=?utf-8?b?" . base64_encode($subject) . "?=";
+
+   $message = "Ім'я: " . $_POST['Name'] . "\nТелефон: " . $_POST['Telephone_number'];
+   $message .= "\nE-mail: " . $_POST['mail'] . "\nIP: " . $_SERVER['REMOTE_ADDR'];
+   $message .= "\nКоментар: " . $_POST['Comment'];
 
    $logMsg = (string)date('l jS \of F Y h:i:s A');
    $logMsg .= '\n\r' . $_POST['mail'] . ' ' . $_POST['Telephone_number'];
